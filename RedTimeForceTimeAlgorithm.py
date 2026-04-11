c6ab3b1348abcd5611bfc2ee47d9bf7c04c9ac2d
kageitachiuchihamorpher <kageitachisharingan@gmail.com>
2026-04-11 18:10:04 -0400

    Complete time algorithm
    This script implements a time algorithm designed to manage human consciousness time limits and explores the relationship between time and energy using the mass of the Earth and the sun.

---
+++ RedTimeForceTimeAlgorithm.py
@@ -0,0 +1,50 @@
+# JunoTimeAlgorithm Version1.0.0.0.2.1.0
+
+# Abstract: This is the original code for the time algorithm. It is designed to administrate time such that it only allows the human consciousness up to 3 hours of "falling out time" before displaying the time as is. After said point... time is time, such that as many and if possible all lives are maintained, effervesecent of the location, position, velocity, acceleration, jerk, thrust, and godspeed sought. That way even women have a fair chance on this "Blue Dot."
+
+# i = 1;
+# j = (i*60);
+# print(str(int(i)) + str(":") + str(int(j)));
+
+i = 1;
+j = (i * 60);
+print(str(int(i)) + str(":") + str(int(j)));
+
+
+## Abstract: This is the updated code for the Pink Time Force Ranger's Morpher so that she may acquire enough time to save her fiancE, as she had originally planned. When that time comes both of them will then be capable of using the Battlizer, as well as her being able to view The Time Force videos in person (alone and without need for the fiance, if and/or when necessary so that she may ascend into super saiyan as planned and to become Iron Bear capable) 
+
+i_initial = 10;
+j_knought = (i_initial * 49);
+print(str(int(i_initial)) + str(":") + str(int(j_knought)));
+
+## Mission complete as intended. The purpose of this program was to create a step forward when calculating time but to also stall such that the number of expanded lives could increase. If God, himself, herself, or even ouivreselves, could be encapsulated into only ten dimensions, then why even allow numbers greater than ten in creation. Even if succeeding numbers were to only be taken as circumstantial, then why ever allow such lessons such as Psalms or maybe even The Book of Revelations?
+## The following is offerred as a Error and Check for the sake of those who are not privy to numbers as the previous demonstration and the following check would allow all to see!
+
+
+
+### Abstract: The following is an experimental attempt to bring a practical application to the usage of the sun and, if possible, other stars into The United States and the worlds' range(s) of control. Its purpose is to describe time and energy in a way that will allow our star to be used as directly as possible in the tracking of time as so far demonstrated above.
+### Captain Junior Ernest Alphonse
+### April 11th, 2026
+### JFK: John F. Kennedy Tales
+### National Security Update
+
+				### A World of Peace Without Danger For All
+
+### Upon discovering more about how the formation of energy over time, America grows even taller, so to speak. Even as many other countries have made advances that have definitely distinguished themselves on the world stage, none have yet to achieve what The United States has recently achieved through The Time-Energy Relationship Equation as found by myself. As will be demonstrated, this update will lead The USA, as well as the entire planet, towards its brighter tomorrow to come.
+### To start off, The Time-Energy Relationship equation will be displayed below to protect the original document from sudden national loss, such as the tragedy of 9/11 or even the BP crisis. Both of these previous tragedies, temporarily so, crippled The United States and resulted in the people losing faith in the national government’s security agendas such as with the TSA for the 9/11 bombers and OSEA as pertaining to the major loss of(…)
+
+###  Mass represented by the mass of the sun, not the mass of any untracked star is displayed below.
+### The speed of light squared is also displayed below.
+
+mass_of_Earth = ((((((1300 - 250)/(3)) - (175))*((3^2)-1))));
+a = (9.8);
+force = (mass_of_Earth * a);
+mass_of_sun = ((3/2) * mass_of_Earth);
+c = (300000);
+
+energy = (mass_of_sun * ((c)^2));
+energy_second_dimensional = (energy * energy);
+energy_third__dimensional = (energy * energy * ((1/2) * (energy)));
+
+time_energy_relationship = ((energy) * (energy) * (energy));
+print(str(int(mass_of_Earth)) + str(":") + str(int(force)) + str(":") + str(int(mass_of_sun)) + str(":") + str(int(energy)) + str(":") + str(int(time_energy_relationship)));
